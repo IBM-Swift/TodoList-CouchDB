77a35dfe85bee0d6c6a897e3af04639bdaf44c9b
jsphyin <jsphyin@gmail.com>
2016-06-13 16:15:28 -0500

    Updated todolist-api version to include version that has user as a field for items

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
     name: "TodoList",
     dependencies: [
-                      .Package(url: "https://github.com/IBM-Swift/todolist-api.git", majorVersion: 0),
+        .Package(url: "https://github.com/IBM-Swift/todolist-api.git", majorVersion: 0, minor: 2),
                       .Package(url: "https://github.com/IBM-Swift/Kitura-CouchDB.git", majorVersion: 0, minor: 16)
     ]
 )
