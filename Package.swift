9f5f838feada005ce1e84f58045be95e6ffa3caf
jsphyin <jsphyin@gmail.com>
2016-06-27 15:36:30 -0500

    Renamed file, made changes to support optional user field

--- Package.swift
+++ Package.swift
@@ -1,7 +1,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "TodoList",
+    name: "TodoListCouchDB",
     dependencies: [
         .Package(url: "https://github.com/IBM-Swift/todolist-api.git", majorVersion: 0, minor: 2),
                       .Package(url: "https://github.com/IBM-Swift/Kitura-CouchDB.git", majorVersion: 0, minor: 16)
