40abe3d049a8b5c29af288431d0f7b5cb873123c
jsphyin <jsphyin@gmail.com>
2016-06-27 16:23:09 -0500

    clearAll() added

--- Package.swift
+++ Package.swift
@@ -1,7 +1,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "TodoListCouchDB",
+    name: "TodoList",
     dependencies: [
         .Package(url: "https://github.com/IBM-Swift/todolist-api.git", majorVersion: 0, minor: 2),
                       .Package(url: "https://github.com/IBM-Swift/Kitura-CouchDB.git", majorVersion: 0, minor: 16)
