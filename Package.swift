2ff1dcc676b02b6ce3b283ed1a19835691a0e685
jsphyin <jsphyin@gmail.com>
2016-06-30 15:08:24 -0500

    Added main file

--- Package.swift
+++ Package.swift
@@ -3,7 +3,11 @@ import PackageDescription
 let package = Package(
     name: "TodoList",
     dependencies: [
-        .Package(url: "https://github.com/IBM-Swift/todolist-api.git", majorVersion: 0, minor: 2),
-                      .Package(url: "https://github.com/IBM-Swift/Kitura-CouchDB.git", majorVersion: 0, minor: 16)
+        /*.Package(url: "https://github.com/IBM-Swift/todolist-api.git", majorVersion: 0),*/
+                      .Package(url: "https://github.com/IBM-Swift/Kitura-CouchDB.git", majorVersion: 0, minor: 19),
+                      .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 0, minor: 19),
+                      .Package(url: "https://github.com/IBM-Swift/HeliumLogger.git", majorVersion: 0, minor: 9),
+                      .Package(url: "https://github.com/IBM-Swift/Swift-cfenv", majorVersion: 1),
+                      .Package(url: "https://github.com/IBM-Swift/todolist-web", majorVersion: 0)
     ]
 )
