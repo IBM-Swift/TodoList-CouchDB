37ef33c2ecc01e5bedd21e1ef70665c152ca48a2
Robert F. Dickerson <rfdickerson@gmail.com>
2016-07-21 18:49:51 -0500

    Started migration to 6-20

--- Package.swift
+++ Package.swift
@@ -3,13 +3,9 @@ import PackageDescription
 let package = Package(
     name: "TodoList",
     dependencies: [
-        .Package(url: "https://github.com/IBM-Swift/Kitura-CouchDB.git",
-                 majorVersion: 0, minor: 19),
-        .Package(url: "https://github.com/IBM-Swift/Kitura.git",
-                 majorVersion: 0, minor: 19),
-        .Package(url: "https://github.com/IBM-Swift/HeliumLogger.git",
-                 majorVersion: 0, minor: 9),
-        .Package(url: "https://github.com/IBM-Swift/todolist-web",
-                 majorVersion: 0, minor: 2)
+        .Package(url: "https://github.com/IBM-Swift/Kitura-CouchDB.git",    majorVersion: 0, minor: 22),
+        .Package(url: "https://github.com/IBM-Swift/Kitura.git",            majorVersion: 0, minor: 22),
+        .Package(url: "https://github.com/IBM-Swift/HeliumLogger.git",      majorVersion: 0, minor: 10),
+        .Package(url: "https://github.com/IBM-Swift/todolist-web",          majorVersion: 0, minor: 3)
     ]
 )
