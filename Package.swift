fb54c647cf6f6f140e072034dd479e0924a77825
Robert F. Dickerson <rfdickerson@gmail.com>
2016-07-21 19:21:23 -0500

    Update HeliumLogger to 0.12

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ let package = Package(
     dependencies: [
         .Package(url: "https://github.com/IBM-Swift/Kitura-CouchDB.git",    majorVersion: 0, minor: 22),
         .Package(url: "https://github.com/IBM-Swift/Kitura.git",            majorVersion: 0, minor: 22),
-        .Package(url: "https://github.com/IBM-Swift/HeliumLogger.git",      majorVersion: 0, minor: 10),
+        .Package(url: "https://github.com/IBM-Swift/HeliumLogger.git",      majorVersion: 0, minor: 12),
         .Package(url: "https://github.com/IBM-Swift/todolist-web",          majorVersion: 0, minor: 3)
     ]
 )
