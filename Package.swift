9c59eb248ccf8efc19b823ca63703791c20036d3
Robert F. Dickerson <rfdickerson@gmail.com>
2016-09-17 21:55:46 -0500

    Update TodoList-Web to 0.7

--- Package.swift
+++ Package.swift
@@ -3,9 +3,8 @@ import PackageDescription
 let package = Package(
     name: "TodoList",
     dependencies: [
-        .Package(url: "https://github.com/IBM-Swift/todolist-web",          majorVersion: 0, minor: 6),
-        .Package(url: "https://github.com/IBM-Swift/Kitura-CouchDB.git",    majorVersion: 0, minor: 30),
-        .Package(url: "https://github.com/IBM-Swift/HeliumLogger.git",      majorVersion: 0, minor: 16),
-        .Package(url: "https://github.com/IBM-Swift/Swift-cfenv.git",       majorVersion: 1, minor: 6)
+        .Package(url: "https://github.com/IBM-Swift/todolist-web",          majorVersion: 0, minor: 7),
+        .Package(url: "https://github.com/IBM-Swift/Kitura-CouchDB.git",    majorVersion: 0, minor: 32),
+        .Package(url: "https://github.com/IBM-Swift/Swift-cfenv.git",       majorVersion: 1, minor: 7)
     ]
 )
