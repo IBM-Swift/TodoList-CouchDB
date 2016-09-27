678b8ab8c0b34033080b9d7e5d1fc90e5c656438
Robert F. Dickerson <rfdickerson@us.ibm.com>
2016-09-27 13:29:47 -0500

    Make promise

--- Sources/TodoList/CouchDBExtensions.swift
+++ Sources/TodoList/CouchDBExtensions.swift
@@ -7,3 +7,24 @@
 //
 
 import Foundation
+import MiniPromiseKit
+import CouchDB
+
+extension CouchDBClient {
+    
+    func dbExists(_ dbName: String) -> Promise<Bool> {
+        return Promise { fulfill, reject in
+            dbExists(dbName) { (result, error) in
+                if error != nil {
+                    reject(error!)
+                } else {
+                    fulfill(result)
+                }
+                
+                
+            }
+        }
+    }
+    
+        
+}
