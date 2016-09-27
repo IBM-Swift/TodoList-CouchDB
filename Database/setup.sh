1c1a6bd9e7cf12b6c885193ae1a7cdc84fe6fa87
Robert F. Dickerson <rfdickerson@us.ibm.com>
2016-09-27 14:12:45 -0500

    Change design name

--- Database/setup.sh
+++ Database/setup.sh
@@ -3,4 +3,4 @@
 # Delete existing database
 curl -u $2:$3 -X DELETE $1/todolist
 curl -u $2:$3 -X PUT $1/todolist
-curl -u $2:$3 -X PUT $1/todolist/_design/example --data-binary @mydesign.json
+curl -u $2:$3 -X PUT $1/todolist/_design/todosdesign --data-binary @mydesign.json
