b041388494699d650654b1420b4f58138e9fc96d
Jessica Pittman <jpittma@us.ibm.com>
2016-10-10 17:51:55 -0500

    Updated package.swift

--- Package.swift
+++ Package.swift
@@ -12,6 +12,7 @@ let package = Package(
         )
     ],
     dependencies: [
+        .Package(url: "https://github.com/IBM-Swift/Kitura.git",        majorVersion: 1, minor: 0),
         .Package(url: "https://github.com/davidungar/miniPromiseKit",       majorVersion: 4, minor: 1),
         .Package(url: "https://github.com/IBM-Swift/Kitura-CouchDB.git",    majorVersion: 1, minor: 0),
         .Package(url: "https://github.com/IBM-Swift/Swift-cfenv.git",       majorVersion: 1, minor: 7)
