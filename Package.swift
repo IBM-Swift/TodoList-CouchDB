5ae1aea05d9acd2ce730f53e3d77e79865111132
jkingoliver <king@us.ibm.com>
2017-03-31 21:25:23 -0500

    update cf deployment tracker

--- Package.swift
+++ Package.swift
@@ -32,6 +32,6 @@ let package = Package(
         .Package(url: "https://github.com/davidungar/miniPromiseKit",           majorVersion: 4),
         .Package(url: "https://github.com/IBM-Swift/Kitura-CouchDB.git",        majorVersion: 1),
         .Package(url: "https://github.com/IBM-Swift/CloudConfiguration.git",    majorVersion: 2),
-        .Package(url: "https://github.com/IBM-Bluemix/cf-deployment-tracker-client-swift.git", majorVersion: 2)
+        .Package(url: "https://github.com/IBM-Bluemix/cf-deployment-tracker-client-swift.git", majorVersion: 3)
     ]
 )
